6e79530e5f36d6aedc4ca9199c36fdab79242cd3
usnar <usna.rafiqzadah@gmail.com>
2023-09-28 17:23:01 -0400

    commiting regularly

--- api/server.js
+++ api/server.js
@@ -1,3 +1,51 @@
-// BUILD YOUR SERVER HERE
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+
+const express = require('express')
+
+const User = require('./users/model')
+
+const server = express();
+
+
+server.get('/api/users', (req, res) => {
+   User.find()
+    .then(users => {
+        res.json(users)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'error getting users', 
+            err: err.message, 
+            stack: err.stack
+        })
+    })
+})
+
+server.get('/api/users/:id', (req, res) => {
+    User.findById(req.params.id)
+     .then(user => {
+        if (!user) {
+            res.status(404).json({
+                message: "does not exist"
+            })
+        }
+        res.status(200).json(user)
+     })
+     .catch(err => {
+         res.status(500).json({
+             message: 'error getting users', 
+             err: err.message, 
+             stack: err.stack
+         })
+     })
+ })
+
+
+
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'not found'
+    })
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
